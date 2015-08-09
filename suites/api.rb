e9ac59b320f4fe17666606d3b7d76b569bef4a42
Colin Dao <quyetdc.uet@gmail.com>
2015-08-09 22:11:20 +0700

    Update api.rb

--- suites/api.rb
+++ suites/api.rb
@@ -2,8 +2,6 @@ pair 1 -- [
 	(
 		"data": {
 			"account_id": [8], 
-			"conversion_account": [5, 6], 
-			"conversion_category": 2,
 			"fb_account_id": [5, 6],
 			"web_account_id": [8]	
 		}		
@@ -14,29 +12,8 @@ pair 1 -- [
 		"message": "ok",		
 		"data":[
 			{
-				"conversion_id": 1,
-				"conversion_category": 1,
-				"conversion_name": "a",
-				"roman_name": "a"								
+				"name": 'test'
 			}
 		]				
-	)
-]
-
-
-pair 2 -- [ 
-	(
-		"data": {
-			"account_id": [8], 
-			"conversion_account": [5, 6], 
-			"conversion_category": 2,
-			"fb_account_id": [5, 6],
-			"web_account_id": [8]	
-		}		
-	)
-
-	(	
-		"status": -1,
-		"message": "[account_id is invalid]"								
 	)
 ]
